9f984dc69f858d3edb7982d5adca920d63091ccf
Code <code@COCTL-41300.local>
2019-11-20 15:10:55 -0800

    added list function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 let keyPress = document.getElementById('keyPress');
+
+let addStuff = document.getElementById('addStuff');
+
+let pCounter = 0;
+
+keyPress.addEventListener('keypress' , function(e){
+
+    if(e.code == 'Enter' && keyPress.value != ''){
+       createListitem(keyPress.value);
+    }
+    
+    
+});
+
+function createListitem(content){
+    let pElement = document.createElement('p');
+
+        pElement.innerText = content;
+        pElement.setAttribute('class', 'list-group-item');
+        pElement.setAttribute('id', pCounter);
+        addStuff.append(pElement);
+        document.getElementById("addStuff").contentEditable = "true";
+        pCounter ++;
+        keyPress.value=null;
+
+}
+
+/*function deleteItem(){
+    .addEventListener('click', function(){
+        this.parentElement.remove();
+    });
+}*/
+
